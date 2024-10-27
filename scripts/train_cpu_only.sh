087fbd3885351c13fc87bb3cecd8229253d00d15
hosjiu <hosjiu1702@gmail.com>
2024-10-27 06:09:27 +0000

    update scripts/train*

--- scripts/train_cpu_only.sh
+++ scripts/train_cpu_only.sh
@@ -1,14 +1,14 @@
-accelerate launch --cpu \
+python -u -m accelerate.commands.launch --cpu \
 train.py \
 --pretrained_ip_adapter_path=checkpoints/ip-adapter-plus_sd15.bin \
 --data_dir=datasets/vitonhd \
 --output_dir=results \
---train_batch_size=4 \
+--train_batch_size=1 \
 --gradient_accumulation_steps=2 \
 --allow_tf32 \
 --use_densepose \
 --mixed_precision=fp16 \
---num_workers=0 \
+--num_workers=4 \
 --num_train_epochs=4 \
 --max_train_steps=12000 \
 --checkpointing_steps=5000
--- scripts/train_single_gpu.sh
+++ scripts/train_single_gpu.sh
@@ -1,14 +1,14 @@
-CUDA_VISIBLE_VISIBLE="0" accelerate launch \
+CUDA_VISIBLE_VISIBLE="0" python -u -m accelerate.commands.launch \
 train.py \
 --pretrained_ip_adapter_path=checkpoints/ip-adapter-plus_sd15.bin \
 --data_dir=datasets/vitonhd \
 --output_dir=results \
---train_batch_size=4 \
+--train_batch_size=2 \
 --gradient_accumulation_steps=2 \
 --allow_tf32 \
 --use_densepose \
 --mixed_precision=fp16 \
---num_workers=0 \
+--num_workers=4 \
 --num_train_epochs=4 \
 --max_train_steps=12000 \
 --checkpointing_steps=5000
