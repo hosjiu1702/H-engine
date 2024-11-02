a20c04c3b2344d2d8988777a1b6a47f37f9272b6
hosjiu <hosjiu1702@gmail.com>
2024-11-02 14:58:17 +0700

    update logic preapre_ip_adapter_image_embeds

--- src/pipelines/tryon.py
+++ src/pipelines/tryon.py
@@ -519,41 +519,15 @@ class TryOnPipeline(
         num_images_per_prompt,
         do_classifier_free_guidance
     ):
-        image_embeds = []
-        if do_classifier_free_guidance:
-            negative_image_embeds = []
+        image_embeds = None
         if ip_adapter_image_embeds is None:
-            # if not isinstance(ip_adapter_image, list):
-            #     ip_adapter_image = [ip_adapter_image]
-
-            # if len(ip_adapter_image) != len(self.unet.encoder_hid_proj.image_projection_layers):
-            #     raise ValueError(
-            #         f"`ip_adapter_image` must have same length as the number of IP Adapters. Got {len(ip_adapter_image)} images and {len(self.unet.encoder_hid_proj.image_projection_layers)} IP Adapters."
-            #     )
-
             output_hidden_state = not isinstance(self.unet.encoder_hid_proj, ImageProjection)
             single_image_embeds, single_negative_image_embeds = self.encode_image(ip_adapter_image, device, 1, output_hidden_state)
-            image_embeds.append(single_image_embeds)
             if do_classifier_free_guidance:
-                negative_image_embeds.append(single_negative_image_embeds)
-        else:
-            for single_image_embeds in ip_adapter_image_embeds:
-                if do_classifier_free_guidance:
-                    single_negative_image_embeds, single_image_embeds = single_image_embeds.chunk(2)
-                    negative_image_embeds.append(single_negative_image_embeds)
-                image_embeds.append(single_image_embeds)
+                image_embeds = torch.cat([single_image_embeds, single_negative_image_embeds], dim=0)
+                image_embeds.to(device=device)
 
-        ip_adapter_image_embeds = []
-        for i, single_image_embeds in enumerate(image_embeds):
-            single_image_embeds = torch.cat([single_image_embeds] * num_images_per_prompt, dim=0)
-            if do_classifier_free_guidance:
-                single_negative_image_embeds = torch.cat([negative_image_embeds[i]] * num_images_per_prompt, dim=0)
-                single_image_embeds = torch.cat([single_negative_image_embeds, single_image_embeds], dim=0)
-
-            single_image_embeds = single_image_embeds.to(device=device)
-            ip_adapter_image_embeds.append(single_image_embeds)
-
-        return ip_adapter_image_embeds[0]
+        return image_embeds
 
     # Copied from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion.StableDiffusionPipeline.run_safety_checker
     def run_safety_checker(self, image, device, dtype):
