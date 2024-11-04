more fix for wrong shape for mask

src/dataset/vitonhd.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 from typing import Text, Union, List
 from pathlib import Path
 import os
@@ -7,11 +6,6 @@ from torch.utils.data import Dataset
 from torchvision.transforms import v2
 from PIL import Image
 from transformers import CLIPImageProcessor
-
-
-@dataclass
-class DownscaleResolution: 
-    resolution = (512, 384)
 
 
 class VITONHDDataset(Dataset):
@@ -25,7 +19,6 @@ class VITONHDDataset(Dataset):
             height: int = 1024,
             width: int = 768,
             use_CLIPVision: bool = True,
-            downscale: bool = False
     ):
         super(VITONHDDataset, self).__init__()
         self.data_rootpath = data_rootpath
@@ -35,7 +28,6 @@ class VITONHDDataset(Dataset):
         self.height = height
         self.width = width
         self.use_CLIPVision = use_CLIPVision
-        self.downscale = downscale
 
         self.totensor = v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)])
         self.transform = v2.Compose(
@@ -70,37 +62,23 @@ class VITONHDDataset(Dataset):
     def __getitem__(self, index):
         item = {}
 
-        img = Image.open(self.im_paths[index])
-        c = Image.open(self.c_paths[index])
-        dp = Image.open(self.dp_paths[index])
-        mask = Image.open(self.m_paths[index]).convert('L')
-        masked_img = Image.open(self.agn_paths[index])
-
-        if self.downscale:
-            new_size = DownscaleResolution.resolution
-
-            img = img.resize(new_size)
-            c = c.resize(new_size)
-            dp = dp.resize(new_size)
-            mask = mask.resize(new_size)
-            masked_img = masked_img.resize(new_size)
-
-            self.height = new_size[0]
-            self.width = new_size[1]
-
         # Person image
+        img = Image.open(self.im_paths[index])
         origin_img = img = img.resize((self.width, self.height))
         img = self.transform(img)
 
         # Cloth
+        c = Image.open(self.c_paths[index])
         c = self.image_processor(images=c, return_tensors='pt').pixel_values
         c = c.squeeze(0)
         
         # Densepose
+        dp = Image.open(self.dp_paths[index])
         dp = dp.resize((self.width, self.height))
         dp = self.transform(dp)
 
         # Mask
+        mask = Image.open(self.m_paths[index]).convert('L')
         mask = mask.resize((self.width, self.height))
         mask = self.totensor(mask)
         # A dirty snippet code that check if this is a binary matrix
@@ -110,6 +88,7 @@ class VITONHDDataset(Dataset):
         # assert len(uni_elems) == 2
 
         # Masked image (agnostic image)
+        masked_img = Image.open(self.agn_paths[index])
         masked_img = masked_img.resize((self.width, self.height))
         masked_img = self.transform(masked_img)
 

train.py
@@ -404,15 +404,17 @@ def main():
     )
     
     # Load dataset for training
+    if args.downscale:
+        vars(args)['height'] = args.height // 2
+        vars(args)['width'] = args.width // 2
     train_dataset = VITONHDDataset(
         data_rootpath=args.data_dir,
         use_trainset=True,
         use_paired_data=True,
         use_augmentation=False,
-        height=args.height,
-        width=args.width,
+        height=args.height
+        width=args.width
         use_CLIPVision=True,
-        downscale=args.downscale
     )
 
     if args.use_subset:
@@ -506,8 +508,8 @@ def main():
                 densepose = vae.encode(batch['densepose'].to(dtype=weight_dtype)).latent_dist.sample()
                 densepose = densepose * vae.config.scaling_factor
                 masks = batch['mask'].to(dtype=weight_dtype)
-                masks = F.interpolate(masks, size=(latents.shape[-2] // 8, latents.shape[-1] // 8))
-
+                masks = F.interpolate(masks, size=(args.height // 8, args.width // 8))
+                
                 # Get text condition
                 # we set input text prompts as a list of empty strings
                 # text_prompts = ['']*len(batch['captions'])