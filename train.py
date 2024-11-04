fix wrong shape for mask

train.py
@@ -506,8 +506,8 @@ def main():
                 densepose = vae.encode(batch['densepose'].to(dtype=weight_dtype)).latent_dist.sample()
                 densepose = densepose * vae.config.scaling_factor
                 masks = batch['mask'].to(dtype=weight_dtype)
-                masks = F.interpolate(masks, size=(args.height//8, args.width//8))
-                
+                masks = F.interpolate(masks, size=(latents.shape[-2] // 8, latents.shape[-1] // 8))
+
                 # Get text condition
                 # we set input text prompts as a list of empty strings
                 # text_prompts = ['']*len(batch['captions'])