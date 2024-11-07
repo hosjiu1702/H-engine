363d2e8583ea1567b4256d1bfb5d0cf3c94f56b9
hosjiu1702 <hosjiu1702@gmail.com>
2024-11-07 02:40:45 +0000

    update scripts/

--- scripts/train_single_gpu_dev.sh
+++ scripts/train_single_gpu_dev.sh
@@ -3,18 +3,18 @@ train_spacat.py \
 --pretrained_ip_adapter_path=checkpoints/ip-adapter-plus_sd15.bin \
 --data_dir=datasets/vitonhd \
 --use_subset \
---num_subset_samples=4 \
+--num_subset_samples=1000 \
 --downscale \
 --output_dir=results \
---train_batch_size=2 \
---gradient_accumulation_steps=1 \
+--train_batch_size=4 \
+--gradient_accumulation_steps=4 \
 --use_densepose \
 --mixed_precision=fp16 \
 --num_workers=8 \
---num_train_epochs=4 \
---max_train_steps=12000 \
---checkpointing_steps=5000 \
---validation_steps=2 \
+--num_train_epochs=100 \
+--max_train_steps=250000 \
+--checkpointing_steps=10000 \
+--validation_steps=10000 \
 --lr=1e-5 \
---use_tracker=false \
+--use_tracker=true \
 --project_name=Full_ft_IPAdapter_UNet
