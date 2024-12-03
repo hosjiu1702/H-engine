93faa49e63e98524e5f7df6a22c6ab453e960ea7
hosjiu <hosjiu1702@gmail.com>
2024-12-03 20:50:41 +0700

    add logic for creating masked image

--- scripts/generate_vitonhd_maskv2.py
+++ scripts/generate_vitonhd_maskv2.py
@@ -2,6 +2,8 @@ import os
 import argparse
 from PIL import Image
 from tqdm.auto import trange, tqdm
+import numpy as np
+from einops import rearrange
 from src.utils.mask_v2 import Maskerv2
 from src.utils import get_project_root
 
@@ -36,12 +38,26 @@ def main():
     for mode, path in tqdm(image_path.items()):
         img_path = os.path.join(path, 'image')
         mask_path = os.path.join(path, args.output_dir)
+        masked_img_path = os.path.join(path, 'agnostic-v2')
         os.makedirs(mask_path, exist_ok=True)
+        os.makedirs(masked_img_path, exist_ok=True)
         for fname in tqdm(os.listdir(img_path), desc=mode):
             fpath = os.path.join(img_path, fname)
             img = Image.open(fpath)
-            mask = masker.create_mask(img)
-            mask.save(os.path.join(mask_path, fname))
+            mask = None
+            saved_path = os.path.join(mask_path, fname)
+            if not os.path.isfile(saved_path):
+                mask = masker.create_mask(img)
+                mask.save(saved_path)
+            mask = mask if mask else Image.open(saved_path)
+            mask_np = np.array(mask)
+            mask_np = np.stack([mask_np] * 3)
+            mask_np = rearrange(mask_np, 'c h w -> h w c')
+            img_np = np.array(img)
+            masked_img_np = np.where(mask_np, np.ones_like(mask_np) * 127, img_np)
+            masked_img = Image.fromarray(masked_img_np)
+            saved_path = os.path.join(masked_img_path, fname)
+            masked_img.save(saved_path)
 
 
 if __name__ == '__main__':
