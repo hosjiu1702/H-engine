79678dd827570ab748d0d2f282894903cc937e61
hosjiu1702 <hosjiu1702@gmail.com>
2024-12-12 07:44:13 +0000

    add test script to train spacat

---
+++ scripts/train_spacat_test.sh
@@ -0,0 +1,26 @@
+# export TORCH_DISTRIBUTED_DEBUG=INFO
+export MIXED_PRECISION_TRAINING='bf16'
+export NUM_GPUS=1
+export NUM_NODES=1
+
+CUDA_VISIBLE_VISIBLE="0" python -u -m accelerate.commands.launch --mixed_precision=$MIXED_PRECISION_TRAINING --num_processes=$NUM_GPUS --num_machines=$NUM_NODES --dynamo_backend='no' \
+train_spacat.py \
+--data_dir=datasets/vitonhd \
+--use_subset \
+--num_subset_samples=50 \
+--downscale \
+--output_dir='results/test' \
+--train_batch_size=2 \
+--gradient_accumulation_steps=1 \
+--use_densepose \
+--mixed_precision='bf16' \
+--snr_gamma='5.0' \
+--num_workers=4 \
+--num_train_epochs=100 \
+--max_train_steps=250000 \
+--checkpointing_steps=10000 \
+--validation_steps=4 \
+--lr=1e-5 \
+--use_tracker=false \
+--save \
+--project_name=Test
